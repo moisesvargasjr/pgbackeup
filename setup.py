d968fcf2be0d239f1c7977f551e0f9cd6f055da9
moisesvargasjr <moisesvargasjr@gmail.com>
2019-11-20 15:44:36 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='pgbackup',
+        version='0.1.0',
+        author='Moises Vargas',
+        author_email='moisesvargasjr@gmail.com',
+        description='A utility for backing up PostgreSQL databases',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/moisesvargasjr/pgbackup',
+        packages=find_packages('src')
